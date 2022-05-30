773b4bf8c8339d05cf65b8cded39a9c481743fe4
Chelnoque <c-thun@yandex.ru>
2022-05-30 21:28:39 +0700

    insta recipes are instant; linked recipes chances are expressions

--- TheRoost/TheWorld - Local Applications/RecipeLinksMaster.cs
+++ TheRoost/TheWorld - Local Applications/RecipeLinksMaster.cs
@@ -6,35 +6,58 @@ using SecretHistories.Commands.SituationCommands;
 using SecretHistories.Core;
 using SecretHistories.Fucine;
 
+using Roost.Twins.Entities;
+
 namespace Roost.World.Recipes
 {
     public static class RecipeLinkMaster
     {
-        public static Action<Situation, Recipe, Expulsion, FucinePath> SpawnNewSituation;
-        private static Situation currentSituation;
-        private static List<Recipe> xtriggerLinks = new List<Recipe>();
-
+        public readonly static Action<Situation, Recipe, Expulsion, FucinePath> SpawnNewSituation = Delegate.CreateDelegate(typeof(Action<Situation, Recipe, Expulsion, FucinePath>), typeof(Situation).GetMethodInvariant("SpawnNewSituation")) as Action<Situation, Recipe, Expulsion, FucinePath>;
+        private static readonly List<Recipe> xtriggerLinks = new List<Recipe>();
+        const string CHANCE = "chance";
         internal static void Enact()
         {
+            //instant recipes are instant
             Machine.Patch(
-                original: typeof(AttemptAspectInductionCommand).GetMethodInvariant(nameof(AttemptAspectInductionCommand.Execute)),
-                prefix: typeof(RecipeLinkMaster).GetMethodInvariant(nameof(StoreSituationFromWhichInductionHappens)));
+                original: typeof(Situation).GetMethodInvariant(nameof(Situation.TransitionToState)),
+                postfix: typeof(RecipeLinkMaster).GetMethodInvariant(nameof(ProceedWithInstantRecipe)));
 
+            //inductions obey reqs and uses explusions
             Machine.Patch(
                 original: typeof(AttemptAspectInductionCommand).GetMethodInvariant("PerformAspectInduction"),
                 prefix: typeof(RecipeLinkMaster).GetMethodInvariant(nameof(PerformAspectInduction)));
 
+            //xtrigger links are evaluated
             Machine.Patch(
                 original: typeof(RecipeConductor).GetMethodInvariant(nameof(RecipeConductor.GetLinkedRecipe)),
                 prefix: typeof(RecipeLinkMaster).GetMethodInvariant(nameof(CheckXtriggerLinks)));
 
-            SpawnNewSituation = Delegate.CreateDelegate(typeof(Action<Situation, Recipe, Expulsion, FucinePath>), typeof(Situation).GetMethodInvariant("SpawnNewSituation")) as Action<Situation, Recipe, Expulsion, FucinePath>;
+            Machine.ClaimProperty<LinkedRecipeDetails, Funcine<int>>(CHANCE, false, "100");
+            Machine.Patch(
+                original: typeof(LinkedRecipeDetails).GetPropertyInvariant(nameof(LinkedRecipeDetails.Chance)).GetGetMethod(),
+                prefix: typeof(RecipeLinkMaster).GetMethodInvariant(nameof(GetRefRecipeChance)));
+            Machine.Patch(
+                original: typeof(LinkedRecipeDetails).GetMethodInvariant(nameof(LinkedRecipeDetails.ShouldAlwaysSucceed)),
+                prefix: typeof(RecipeLinkMaster).GetMethodInvariant(nameof(ShouldAlwaysSucceed)));
         }
 
-        //AttemptAspectInductionCommand.Execute()
-        private static void StoreSituationFromWhichInductionHappens(Situation situation)
+        private static void ProceedWithInstantRecipe(Situation __instance)
         {
-            currentSituation = situation;
+            if (__instance.TimeRemaining <= 0f && __instance.StateIdentifier != SecretHistories.Enums.StateEnum.Complete)
+                __instance.State.Continue(__instance);
+        }
+
+        private static bool GetRefRecipeChance(LinkedRecipeDetails __instance, ref int __result)
+        {
+            __result = __instance.RetrieveProperty<Funcine<int>>(CHANCE).value;
+            return false;
+        }
+
+        private static bool ShouldAlwaysSucceed(LinkedRecipeDetails __instance, ref bool __result)
+        {
+            const string maxChance = "100";
+            __result = __instance.Challenges.Count == 0 && __instance.RetrieveProperty<Funcine<int>>(CHANCE).formula == maxChance;
+            return false;
         }
 
         //AttemptAspectInductionCommand.PerformAspectInduction()
@@ -42,11 +65,11 @@ namespace Roost.World.Recipes
         {
             AspectsInContext aspectsInContext = Watchman.Get<HornedAxe>().GetAspectsInContext(situation.GetAspects(true), null);
             foreach (LinkedRecipeDetails linkedRecipeDetails in aspectElement.Induces)
-                if (Watchman.Get<IDice>().Rolld100(null) <= linkedRecipeDetails.Chance)
+                if (linkedRecipeDetails.Chance < UnityEngine.Random.Range(1, 101))
                 {
                     Recipe recipe = Watchman.Get<Compendium>().GetEntityById<Recipe>(linkedRecipeDetails.Id);
                     if (recipe.RequirementsSatisfiedBy(aspectsInContext))
-                        SpawnNewSituation(currentSituation, recipe, linkedRecipeDetails.Expulsion, FucinePath.Current());
+                        SpawnNewSituation(situation, recipe, linkedRecipeDetails.Expulsion, FucinePath.Current());
                 }
 
             return false;
@@ -84,9 +107,9 @@ namespace Roost
 
         public static void PushXtriggerLink(string recipeId, int orderShift = 0)
         {
-            Recipe recipe = Watchman.Get<Compendium>().GetEntityById<Recipe>(recipeId);
+            Recipe recipe = Machine.GetEntity<Recipe>(recipeId);
             if (recipeId == null)
-                Birdsong.Sing("Trying to push non-existed recipe link '{0}'", recipeId);
+                Birdsong.Sing($"Trying to push non-existed recipe link '{recipeId}'");
             else
                 PushXtriggerLink(recipe, orderShift);
         }
