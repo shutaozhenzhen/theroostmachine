55d539bae3c93a856fa19612bee213b6760409be
Chelnoque <c-thun@yandex.ru>
2022-06-10 19:59:52 +0700

    levers stuff

--- TheRoost/Elegiast - Achievements/ElegiastArchive.cs
+++ TheRoost/Elegiast - Achievements/ElegiastArchive.cs
@@ -8,14 +8,31 @@ namespace Roost.Elegiast
 {
     internal static class ElegiastArchive
     {
-        internal static void SetValue(string key, string value)
+        private static readonly List<string> permanentLevers = new List<string>();
+
+        internal static void MarkLeverAsPermanent(string levers)
         {
-            Watchman.Get<Stable>().Protag().SetOrOverwritePastLegacyEventRecord(key, value);
+            permanentLevers.Add(levers);
         }
 
-        internal static string GetValue(string key)
+        internal static void SetLeverPast(string lever, string value)
         {
-            return Watchman.Get<Stable>().Protag().GetPastLegacyEventRecord(key);
+            Watchman.Get<Stable>().Protag().SetOrOverwritePastLegacyEventRecord(lever, value);
+        }
+
+        internal static void SetLeverFuture(string lever, string value)
+        {
+            Watchman.Get<Stable>().Protag().SetOrOverwritePastLegacyEventRecord(lever, value);
+        }
+
+        internal static string GetLeverPast(string lever)
+        {
+            return Watchman.Get<Stable>().Protag().GetPastLegacyEventRecord(lever);
+        }
+
+        internal static string GetLeverFuture(string lever)
+        {
+            return Watchman.Get<Stable>().Protag().GetFutureLegacyEventRecord(lever);
         }
     }
 }
@@ -24,14 +41,24 @@ namespace Roost
 {
     public static partial class Machine
     {
-        public static void SetGlobalValue(string key, string value)
+        public static void SetLeverPast(string lever, string value)
         {
-            Roost.Elegiast.ElegiastArchive.SetValue(key, value);
+            Roost.Elegiast.ElegiastArchive.SetLeverPast(lever, value);
         }
 
-        public static string GetGlobalValue(string key)
+        public static string GetLeverPast(string lever)
         {
-            return Roost.Elegiast.ElegiastArchive.GetValue(key);
+            return Roost.Elegiast.ElegiastArchive.GetLeverPast(lever);
+        }
+
+        public static void SetLeverFuture(string lever, string value)
+        {
+            Roost.Elegiast.ElegiastArchive.SetLeverFuture(lever, value);
+        }
+
+        public static string GetLeverFuture(string lever)
+        {
+            return Roost.Elegiast.ElegiastArchive.GetLeverFuture(lever);
         }
     }
 }
